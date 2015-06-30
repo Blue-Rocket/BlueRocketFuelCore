bd214a15d963548a22f2cfff7cde343a84c8dfb6
Shawn McKee <shawnmckee@mac.com>
2015-06-30 08:26:32 -0700

    reset password webservice support

---
+++ BlueRocketFuelCore/webservice/requests/resetPassword/ResetPasswordWebServiceRequest.h
@@ -0,0 +1,11 @@
+//
+//  Created by Shawn McKee on 2/18/15.
+//  Copyright (c) 2015 soulsend. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BRAuthenticatedWebServiceRequest.h"
+
+@interface ResetPasswordWebServiceRequest : BRAuthenticatedWebServiceRequest
+
+@end
---
+++ BlueRocketFuelCore/webservice/requests/resetPassword/ResetPasswordWebServiceRequest.m
@@ -0,0 +1,10 @@
+//
+//  Created by Shawn McKee on 2/18/15.
+//  Copyright (c) 2015 soulsend. All rights reserved.
+//
+
+#import "ResetPasswordWebServiceRequest.h"
+
+@implementation ResetPasswordWebServiceRequest
+
+@end
