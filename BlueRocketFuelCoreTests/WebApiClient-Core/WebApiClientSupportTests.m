8c9aee33f6f6b7e8574f7f067e517c2b9fc7d5af
Matt Magoffin <git+matt@msqr.us>
2015-08-18 14:17:56 +1200

    Start work on WebApiClientSupport unit tests.

--- BlueRocketFuelCoreTests/WebApiClient-Core/WebApiClientSupportTests.m
+++ BlueRocketFuelCoreTests/WebApiClient-Core/WebApiClientSupportTests.m
@@ -11,6 +11,7 @@
 #define HC_SHORTHAND
 #import <OCHamcrest/OCHamcrest.h>
 
+#import "BRAppUser.h"
 #import "WebApiClientSupport.h"
 
 @interface TestWebApiClient : WebApiClientSupport
@@ -35,6 +36,14 @@
 
 @end
 
+@interface TestUser : BRAppUser
+
+@property (nonatomic, strong) NSString *subclassProp;
+
+@end
+
+#pragma mark - Unit tests
+
 @interface WebApiClientSupportTests : BaseNetworkTestingSupport
 
 @end
@@ -49,8 +58,41 @@
 	client = [[TestWebApiClient alloc] initWithEnvironment:self.testEnvironment];
 }
 
+- (void)testBaseApiURL {
+	NSURL *baseURL = [client baseApiURL];
+	assertThat(baseURL, equalTo([NSURL URLWithString:@"http://localhost/"]));
+}
+
 - (void)testLoadRoutes {
-	assertThat(client.routeNames, containsInAnyOrder(@"register", @"login", nil));
+	assertThat(client.routeNames, containsInAnyOrder(@"register", @"login", @"user", nil));
+	id<WebApiRoute> route = [client routeForName:@"register" error:nil];
+	assertThat(route, notNilValue());
+	assertThat(route.name, equalTo(@"register"));
+	assertThat(route.path, equalTo(@"register"));
+	assertThat(route.method, equalTo(@"POST"));
+	assertThat(route.dataMapper, equalTo(@"RestKitWebApiDataMapper"));
+	assertThat(route[@"dataMapperRequestRootKeyPath"], equalTo(@"user"));
+	assertThatBool(route.preventUserInteraction, isTrue());
+	
+	route = [client routeForName:@"login" error:nil];
+	assertThat(route, notNilValue());
+	assertThat(route.name, equalTo(@"login"));
+	assertThat(route.path, equalTo(@"login"));
+	assertThat(route.method, equalTo(@"POST"));
+	assertThat(route.dataMapper, equalTo(@"RestKitWebApiDataMapper"));
+	assertThat(route[@"dataMapperRequestRootKeyPath"], equalTo(@"user"));
+	assertThatBool(route.preventUserInteraction, isTrue());
+}
+
+- (void)testURLPathVariable {
+	id<WebApiRoute> route = [client routeForName:@"user" error:nil];
+	NSURL *url = [client URLForRoute:route pathVariables:@{ @"userId" : @(1234) } parameters:nil error:nil];
+	assertThat([url absoluteString], equalTo(@"http://localhost/user/1234"));
+}
+
+- (void)dictionaryFromUserObject {
+	BRAppUser *user = nil;
+	NSDictionary *dictionary = [client dictionaryForParametersObject:user];
 }
 
 @end
