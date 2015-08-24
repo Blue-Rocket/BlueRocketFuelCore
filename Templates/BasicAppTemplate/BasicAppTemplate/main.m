f6826c20d559e3dd3273c91631970593521493b3
Matt Magoffin <git+matt@msqr.us>
2015-08-24 20:38:31 +1200

    Fix Podfile; add logging to main.m.

--- Templates/BasicAppTemplate/BasicAppTemplate/main.m
+++ Templates/BasicAppTemplate/BasicAppTemplate/main.m
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <BRCocoaLumberjack/BRCocoaLumberjack.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
 	@autoreleasepool {
+		BRLoggingSetupDefaultLogging();
 	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 	}
 }
--- Templates/BasicAppTemplate/Resources/increment-build-version.sh
+++ Templates/BasicAppTemplate/Resources/increment-build-version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ ${CONFIGURATION} == "Release" ]; then
 	buildNumber=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFOPLIST_FILE"`
 	buildNumber=$(($buildNumber + 1))
@@ -6,7 +6,10 @@ if [ ${CONFIGURATION} == "Release" ]; then
 	gitCommit=`git rev-parse --short HEAD`
 	gitTag=`git tag --points-at HEAD`
 	/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "$INFOPLIST_FILE"
-	/usr/libexec/PlistBuddy -c "Set :X-SCM-Branch $gitBranch" "$INFOPLIST_FILE"
-	/usr/libexec/PlistBuddy -c "Set :X-SCM-Commit $gitCommit" "$INFOPLIST_FILE"
-	/usr/libexec/PlistBuddy -c "Set :X-SCM-Tag $gitTag" "$INFOPLIST_FILE"
+	/usr/libexec/PlistBuddy -c "Add :X-SCM-Branch string $gitBranch" "$INFOPLIST_FILE" \
+		|| /usr/libexec/PlistBuddy -c "Set :X-SCM-Branch $gitBranch" "$INFOPLIST_FILE"
+	/usr/libexec/PlistBuddy -c "Add :X-SCM-Commit string $gitCommit" "$INFOPLIST_FILE" \
+		|| /usr/libexec/PlistBuddy -c "Set :X-SCM-Commit $gitCommit" "$INFOPLIST_FILE"
+	/usr/libexec/PlistBuddy -c "Add :X-SCM-Tag string $gitTag" "$INFOPLIST_FILE" \
+		|| /usr/libexec/PlistBuddy -c "Set :X-SCM-Tag $gitTag" "$INFOPLIST_FILE"
 fi
