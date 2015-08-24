939845779a530b98939d68ab0314f074e43eda01
Matt Magoffin <git+matt@msqr.us>
2015-08-24 17:41:01 +1200

    New single-view project creation to start template.

---
+++ Templates/BasicAppTemplate/BasicAppTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicAppTemplate
+//
+//  Created by Matt on 24/08/15.
+//  Copyright (c) 2015 MyOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
