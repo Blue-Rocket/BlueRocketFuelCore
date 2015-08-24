a1ada9672bc592d8756310bca0092d627a46555f
Matt Magoffin <git+matt@msqr.us>
2015-08-25 06:53:41 +1200

    Configure logging for debug builds.

--- Templates/BasicAppTemplate/BasicAppTemplate/AppDelegate.m
+++ Templates/BasicAppTemplate/BasicAppTemplate/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import <BRCocoaLumberjack/BRCocoaLumberjack.h>
+
 @interface AppDelegate ()
 
 @end
@@ -16,7 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	// Override point for customization after application launch.
+	log4Debug(@"Hi there!");
 	return YES;
 }
 
