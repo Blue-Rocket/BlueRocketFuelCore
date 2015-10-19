544aad8b31d4adfc49e2477049177406e27dc007
Matt Magoffin <git+matt@msqr.us>
2015-10-19 16:24:30 +1300

    Fix broken RestKit pod (broken by Cocoapods 0.39).

--- BRFCore/Pods/RestKit/Code/ObjectMapping/RKObjectMapping.h
+++ BRFCore/Pods/RestKit/Code/ObjectMapping/RKObjectMapping.h
@@ -18,8 +18,8 @@
 //  limitations under the License.
 //
 
-#import "RKMacros.h"
-#import "RKMapping.h"
+#import "Support/RKMacros.h"
+#import "ObjectMapping/RKMapping.h"
 
 #import <RKValueTransformers/RKValueTransformers.h>
 
